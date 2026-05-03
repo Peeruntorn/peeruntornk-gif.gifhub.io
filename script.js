da56407a18a20b82e4575a78610a2d3bc02ffa6f
peeruntornk-gif <peeruntornk@gmail.com>
2026-05-03 07:48:51 +0000

    Create Portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+const navLinks = document.querySelectorAll('.top-nav a');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 20,
+        behavior: 'smooth',
+      });
+    }
+  });
+});
